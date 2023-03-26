37ef882f846f0edeee7e3b1784e04a71d5f3bfa4
Victor Rodrigues da Silva <victor.vki.dasilva@gmail.com>
2023-03-26 18:40:25 -0300

    making input communicate mk1

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,48 +1,78 @@
 <template>
-    <div class="loginPage">
+  <div class="loginPage">
+    <form @submit="redirect($event)">
       <div class="box">
         <h1>Login</h1>
-        <Input></Input>
-        <Input></Input>
+        <InputText class="text-field" v-model="login"></InputText>
+        <!-- <InputText class="text-field"></InputText> -->
+        <input type="text" v-model="senha"/>
+        <InputSubmit class="submit-btn"></InputSubmit>
       </div>
-    </div>
-  </template>
+    </form>
+  </div>
+</template>
   
-  <script>
-  import Input from '../components/Input.vue';
-  
-  export default {
-    components: {
-      Input
-    },
+<script>
+import InputSubmit from '../components/InputSubmit.vue';
+import InputText from '../components/InputText.vue';
+
+export default {
+  data() {
+    return {
+      login: null,
+      senha: null
+    }
+  },
+  components: {
+    InputText,
+    InputSubmit
+  },
+  methods: {
+    redirect(e) {
+
+      e.preventDefault();
+      
+      const data = {
+        login: this.login,
+        senha: this.senha
+      }
+      console.log(data)
+      /* this.$router.push('/') */
+    }
   }
-  </script>
+}
+</script>
   
-  <style scoped>
-  .box {
-    display: flex;
-    justify-content: center;
-    flex-direction: column;
-  }
-  
-  input + input {
-    margin: 4vh 0 0 0;
-  }
-  
-  h1 {
-    padding-left: 20px;
-    margin: 0;
-    font-family: 'Courier New', Courier, monospace;
-  }
-  
-  Input {
-    width: 80%;
-    align-self: center;
-  }
-  
-  .loginPage {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-  }
-  </style>
+<style scoped>
+.box {
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+}
+
+input+input {
+  margin: 4vh 10px 0 10px;
+}
+
+h1 {
+  padding-left: 20px;
+  margin: 0;
+  font-family: 'Courier New', Courier, monospace;
+}
+
+.text-field {
+  width: 80%;
+  align-self: center;
+}
+
+.submit-btn {
+  align-self: flex-end;
+  margin-right: 20px;
+}
+
+.loginPage {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
