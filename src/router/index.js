69dd8b69d5d20294d0b3c962832aa1b67406cd0b
Kenzo Yves Yamashita Nobre <kenzonobre@gmail.com>
2023-05-29 15:39:44 -0300

    Adding ProductQuantity component

--- src/router/index.js
+++ src/router/index.js
@@ -94,6 +94,15 @@ const router = createRouter({
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/ProductsView.vue')
     }
+    ,
+    {
+      path: '/ProductQuantity',
+      name: 'ProductQuantity',
+      // route level code-splitting
+      // this generates a separate chunk (login.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/ProductQuantityView.vue')
+    }
   ]
 })
 
