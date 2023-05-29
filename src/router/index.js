a3c4734f75d6279083d61d339518eb60f1257ac2
Victor Rodrigues da Silva <victor.vki.dasilva@gmail.com>
2023-05-29 13:47:29 -0300

    creating products page

--- src/router/index.js
+++ src/router/index.js
@@ -85,6 +85,15 @@ const router = createRouter({
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/AdminMenuView.vue')
     }
+    ,
+    {
+      path: '/products',
+      name: 'products',
+      // route level code-splitting
+      // this generates a separate chunk (login.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/ProductsView.vue')
+    }
   ]
 })
 
