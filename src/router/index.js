60d4839a9f975634cf0d0dfdd5aaa9f8323fc6b9
Kenzo Yves Yamashita Nobre <kenzonobre@gmail.com>
2023-05-30 11:16:45 -0300

    Deleting ProductQuantityView.vue

--- src/router/index.js
+++ src/router/index.js
@@ -96,15 +96,6 @@ const router = createRouter({
     }
     ,
     {
-      path: '/ProductQuantity',
-      name: 'ProductQuantity',
-      // route level code-splitting
-      // this generates a separate chunk (login.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/ProductQuantityView.vue')
-    }
-    ,
-    {
       path: '/Cart',
       name: 'Cart',
       // route level code-splitting
