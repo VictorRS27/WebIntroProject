fdf685568176969b701636c6bc2613d4e3cde5ca
otavio <otaviocoletti@outlook.com>
2023-05-30 10:27:55 -0300

    mais um compenente eliminado create product agora somente view

--- src/views/CreateProductView.vue
+++ src/views/CreateProductView.vue
@@ -1,22 +1,59 @@
 <template>
-    <Navbar />
-    <CreateProduct/>
+  <Navbar />
+
+    <div class="box">
+      <h2>Create Product</h2>
+
+      <div class="side_form">
+        <InputText label="Nome" placeholder="A pet friend product" v-model:value="productName" />
+        <InputText label="Preço" placeholder="A pet friend product" v-model:value="productPrice" />
+      </div>
+      <InputText label="Descrição" placeholder="A pet friend product" v-model:value="productDescription" />
+
+      <div class="form-group">
+        <label for="price">Price:</label>
+        <input type="number" id="price" placeholder="Enter product price" v-model="productPrice" />
+      </div>
+
+      <div class="form-group">
+        <label for="description">Description:</label>
+        <textarea id="description" placeholder="Enter product description" v-model="productDescription"></textarea>
+      </div>
+
+      <div class="form-group">
+        <label for="photos">Photos:</label>
+        <input type="file" id="photos" @change="handlePhotoUpload" multiple />
+        <div class="photos-preview">
+          <div v-for="(photo, index) in photos" :key="index" class="photo">
+            <img :src="photo" alt="Product Photo" />
+            <button class="delete-button" @click="deletePhoto(index)">Delete</button>
+          </div>
+        </div>
+        <button class="upload-button">Upload Photos</button>
+      </div>
+
+      <div class="form-group">
+        <label for="quantity">Quantity in Stock:</label>
+        <input type="number" id="quantity" placeholder="Enter quantity in stock" v-model="quantityInStock" />
+      </div>
+
+      <button class="submit-button">Submit</button>
+    </div>
 </template>
   
 
 <script>
+import InputText from '../components/InputText.vue';
 import Navbar from '../components/Navbar.vue'
 import Footer from '../components/Footer.vue'
-import CreateProduct from '../components/CreateProduct.vue';
-
 
 export default {
-    components: {
-        Navbar,
-        Footer,
-        CreateProduct
-    },
-    data() {
+  components: {
+    Navbar,
+    Footer,
+    InputText
+  },
+  data() {
     return {
       productName: '',
       productPrice: '',
@@ -44,13 +81,24 @@ export default {
 }
 </script>
 <style scoped>
-
-.box{
-    margin-top: 12vh;
+.box {
+  margin-top: 15vh;
+  margin-bottom: 5vh;
 }
+
 .create-products {
-  max-width: 500px;
-  margin-top: 1vh;
+  height: 70%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.side_form {
+  display: flex;
+}
+
+.side_form input {
+  margin: 10px;
 }
 
 h2 {
@@ -58,7 +106,7 @@ h2 {
 }
 
 .form-group {
-  margin-bottom: 20px;
+  margin-bottom: 0px;
 }
 
 label {
@@ -70,7 +118,7 @@ input[type="text"],
 input[type="number"],
 textarea {
   width: 100%;
-  padding: 10px;
+  padding: 5px;
   border: 1px solid #ccc;
   border-radius: 4px;
 }
--- src/views/EventsView.vue
+++ src/views/EventsView.vue
@@ -1,14 +1,26 @@
 <template>
     <div class="container">
-        <Navbar/>
-        <Events/>
+        <Navbar />
+        <div class="posts">
+
+            <div v-for="post in posts" :key="post.id" class="post">
+                <h3>{{ post.title }}</h3>
+                <div id="post_back">
+                    <p id="post_text">{{ post.text }}</p>
+                </div>
+
+                <p class="date">{{ formatDate(post.date) }}</p>
+                <p><strong>Address:</strong> {{ post.address }}</p>
+                <p><strong>Type:</strong> {{ post.type }}</p>
+            </div>
+
+        </div>
         <!-- <Footer/> -->
     </div>
 </template>
 
 <script>
 
-import Events from '../components/Events.vue'
 import Navbar from '../components/Navbar.vue'
 import Footer from '../components/Footer.vue'
 
@@ -16,10 +28,45 @@ import Footer from '../components/Footer.vue'
 export default {
     components: {
         Navbar,
-        Events,
         Footer
+    },
+    name: "Events",
+    data() {
+        return {
+            posts: [
+                {
+                    id: 1,
+                    title: 'Post 1',
+                    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+                    date: '2023-05-26',
+                    address: '123 Main St, City, State',
+                    type: 'Conference'
+                },
+                {
+                    id: 2,
+                    title: 'Post 2',
+                    text: 'Pellentesque ac lectus fringilla, lacinia diam a, feugiat tortor.',
+                    date: '2023-05-27',
+                    address: '456 Elm St, City, State',
+                    type: 'Workshop'
+                },
+                {
+                    id: 3,
+                    title: 'Post 3',
+                    text: 'Maecenas tempor risus non mi ullamcorper finibus.',
+                    date: '2023-05-28',
+                    address: '789 Oak St, City, State',
+                    type: 'Seminar'
+                }
+            ]
+        };
+    },
+    methods: {
+        formatDate(date) {
+            const options = { year: 'numeric', month: 'long', day: 'numeric' };
+            return new Date(date).toLocaleDateString(undefined, options);
+        }
     }
-
 }
 </script>
 <style scoped>
@@ -28,5 +75,38 @@ export default {
     flex-wrap: wrap;
     justify-content: space-evenly;
     align-items: stretch;
+}
+
+.posts {
+    margin: 15vw;
+    display: flex;
+    flex-direction: column;
+}
+
+.post {
+    margin-bottom: 20px;
+}
+
+#post_back {
+    background-color: lightgrey;
+    height: 250px;
+    width: 750px;
+    border-radius: 30px;
+}
+
+#post_text {
+    padding: 20px;
+}
+
+
+
+h3 {
+    font-size: 18px;
+    font-weight: bold;
+}
+
+.date {
+    color: #888;
+    font-size: 14px;
 }
 </style>
