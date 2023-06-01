dc33d7fda8363dd6b25dc4fc01ae3ca770c93b7e
otavio <otaviocoletti@outlook.com>
2023-06-01 00:22:14 -0300

    SOUNNND

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,62 @@
 <template>
-  <div class="app">
-    <RouterView />
+  <div class="app" @mousemove="handleMouseMove">
+    <router-view />
     <Footer></Footer>
-    <Sound/>
   </div>
 </template>
 
 <script>
-import Sound from './components/Sound.vue'
 import Footer from './components/Footer.vue';
 import Navbar from './components/Navbar.vue';
+
 export default {
   components: {
-    Sound,
     Navbar,
     Footer
+  },
+  data() {
+    return {
+      audio: null
+    };
+  },
+  methods: {
+    handleMouseMove() {
+      console.log('Mouse move');
+      console.log(Math.floor(Math.random() * 170) % 170);
+      if(Math.floor(Math.random() * 170) % 170 === 1){
+        this.playSound();
+      }
+    },
+    playSound() {
+      const soundID = Math.floor((Math.random() * 10) % 7);
+      const soundsList = [
+        'public/cat-meow-6226.mp3',
+        'public/zimmermacaco.mp3',
+        'public/sheep-122256.mp3',
+        'public/duck-quack-112941.mp3',
+        'public/elephant-trumpets-growls-6047.mp3',
+        'public/owl-144750.mp3',
+        'public/pet-squeak-toy-81315.mp3'
+      ];
+
+      if (!this.audio) {
+        this.audio = new Audio();
+      }
+
+      this.audio.src = soundsList[soundID]; // Replace with the path to your sound file
+      this.audio.play().catch(error => {
+        // Handle play() promise rejection
+        console.log('Failed to play sound:', error);
+      });
+    }
   }
-}
+};
 </script>
 
 <style>
 body {
   margin: 0%;
   height: 100vh;
-
 }
 
 .app {
--- src/components/Sound.vue
+++ src/components/Sound.vue
@@ -1,57 +1,58 @@
 <template>
-    <!-- <div class="sound-player">
-      <h2>Sound Player</h2>
-      <button class="play-button" @click="playSound">Play Sound</button>
-    </div> -->
-  </template>
-  
-  <script>
+  <div class="sound-player" @mousemove="playSound">
+    <h2>Sound Player</h2>
+    <button class="play-button">Play Sound</button>
+  </div>
+</template>
 
-  export default {
-    name: "Sound",
-    data() {
-      return {
-        audio: new Audio(),
-        interval: null
-      };
-    },
-    mounted() {
-      this.setRandomInterval();
-    },
-    methods: {
-      setRandomInterval() {
-        const minDelay = 4000; // Minimum delay in milliseconds
-        const maxDelay = 10000; // Maximum delay in milliseconds
-        const delay = Math.floor(Math.random() * (maxDelay - minDelay + 1) + minDelay);
-        this.intervalId = delay
-      },
-      playSound() {
-        let soundID = Math.random() % 7
-        const soundsList = ['public/cat-meow-6226.mp3', 'public/zimmermacaco.mp3', 'public/sheep-122256.mp3', 'public/duck-quack-112941.mp3','public/elephant-trumpets-growls-6047.mp3','public/owl-144750.mp3', 'public/pet-squeak-toy-81315.mp3']
-        this.audio.src = soundsList[soundID]; // Replace with the path to your sound file
-        this.audio.play();
-      }
+<script>
+export default {
+  name: "Sound",
+  data() {
+    return {
+      audio: new Audio()
+    };
+  },
+  mounted(){
+    document.addEventListener('mousemove', this.playSound);
+  },
+  methods: {
+    
+    playSound() {
+      console.log("move")
+      const soundID = Math.floor(Math.random() * 7);
+      const soundsList = [
+        "public/cat-meow-6226.mp3",
+        "public/zimmermacaco.mp3",
+        "public/sheep-122256.mp3",
+        "public/duck-quack-112941.mp3",
+        "public/elephant-trumpets-growls-6047.mp3",
+        "public/owl-144750.mp3",
+        "public/pet-squeak-toy-81315.mp3"
+      ];
+      this.audio.src = soundsList[soundID]; // Replace with the path to your sound file
+      this.audio.play();
     }
-  };
-  </script>
-  
-  <style scoped>
-  .sound-player {
-    text-align: center;
   }
-  
-  h2 {
-    margin-bottom: 20px;
-  }
-  
-  .play-button {
-    padding: 10px 20px;
-    background-color: lightgreen;
-    border: none;
-    border-radius: 4px;
-    color: white;
-    font-weight: bold;
-    cursor: pointer;
-  }
-  </style>
-  
+};
+</script>
+
+<style scoped>
+.sound-player {
+  text-align: center;
+}
+
+h2 {
+  margin-bottom: 20px;
+}
+
+.play-button {
+  padding: 10px 20px;
+  background-color: lightgreen;
+  border: none;
+  border-radius: 4px;
+  color: white;
+  font-weight: bold;
+  cursor: pointer;
+}
+</style>
