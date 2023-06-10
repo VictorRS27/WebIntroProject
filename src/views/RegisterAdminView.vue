deb6dac54d0c7d00106eb27c2d6b2dff077809df
Kenzo Yves Yamashita Nobre <kenzonobre@gmail.com>
2023-06-10 17:59:10 -0300

    Updated Register page

--- src/views/RegisterAdminView.vue
+++ src/views/RegisterAdminView.vue
@@ -2,56 +2,68 @@
     <Navbar />
     <div class="container">
         <div class="box">
-
             <h2>Register Admin</h2>
-
+            
             <div class="form-group">
                 <label for="username">Username:</label>
-                <input type="text" id="username" placeholder="Enter username" v-model="username" required/>
+                <input
+                type="text"
+                id="username"
+                placeholder="Enter username"
+                v-model="username"
+                />
             </div>
-
+            
             <div class="form-group">
                 <label for="password">Password:</label>
-                <input type="password" id="password" placeholder="Enter password" v-model="password" required/>
+                <input
+                type="password"
+                id="password"
+                placeholder="Enter password"
+                v-model="password"
+                />
             </div>
-
+            
             <div class="form-group">
                 <label for="email">Email:</label>
-                <input type="email" id="email" placeholder="Enter email" v-model="email" required/>
+                <input
+                type="email"
+                id="email"
+                placeholder="Enter email"
+                v-model="email"
+                />
             </div>
-
+            
             <div class="form-group">
-                <label for="telephone">telephone:</label>
-                <input 
-                type="telephone" 
-                id="telephone" 
-                placeholder="Enter telephone" 
+                <label for="telephone">Telephone:</label>
+                <input
+                type="telephone"
+                id="telephone"
+                placeholder="Enter telephone"
                 v-model="telephone"
-                @input="formatPhoneNumber" 
-                maxlength="15"/>
+                @input="formatPhoneNumber"
+                maxlength="15"
+                />
             </div>
-
-            <p class="warning-text" v-show="showWarning">Email or username already exists.</p>
-
-
-            <button class="submit-button" @click="registerAdmin">Submit</button>
-
+            
+            <p class="warning-text" v-show="showWarning">
+                Email or username already exists.
+            </p>
+            
+            <button class="submit-button" @click="submitForm">Submit</button>
         </div>
     </div>
 </template>
 
 <script>
-
-
-import axios from 'axios';
-import Navbar from '../components/Navbar.vue'
-import Footer from '../components/Footer.vue'
-
+import axios from "axios";
+import Navbar from "../components/Navbar.vue";
+import Footer from "../components/Footer.vue";
 
 export default {
     components: {
         Navbar,
-        Footer
+        Footer,
     },
     name: "RegisterAdmin",
     data() {
@@ -60,116 +72,102 @@ export default {
             password: "",
             email: "",
             telephone: "",
-            showWarning: false, // New data property for showing the warning text
+            showWarning: false,
         };
-    },
-    props: {
-        item_id: {
-            default: -1
-        }
-    },
-    mounted() {
-        let Mycookie = document.cookie;
-
-        if (Mycookie == "" || (Mycookie != "" && Mycookie[0] != 'a')) {
-            this.$router.push('/error');
-        }
-        this.load_edit()
     },
     methods: {
         formatPhoneNumber() {
-            let value = this.telephone
-            value = value.replace(/\D/g, '')
-            value = value.replace(/(\d{2})(\d)/, "($1) $2")
-            value = value.replace(/(\d)(\d{4})$/, "$1-$2")
-            this.telephone = value
+            let value = this.telephone;
+            value = value.replace(/\D/g, "");
+            value = value.replace(/(\d{2})(\d)/, "($1) $2");
+            value = value.replace(/(\d)(\d{4})$/, "$1-$2");
+            this.telephone = value;
         },
-        load_edit() {
-            console.log(this.item_id)
-            if (this.item_id !== -1) {
-                axios.get("http://localhost:3000/admin")
+        submitForm() {
+            if (
+            this.username != "" &&
+            this.password != "" &&
+            this.email != "" &&
+            this.telephone != ""
+            ) {
+                console.log("username = ", this.username);
+                console.log("password = ", this.password);
+                console.log("email = ", this.email);
+                console.log("telephone = ", this.telephone);
+                const newAdmin = {
+                    username: this.username,
+                    password: this.password,
+                    email: this.email,
+                    telephone: this.telephone,
+                };
+                
+                axios
+                .get("http://localhost:3000/admin")
+                .then((response) => {
+                    const admins = response.data;
+                    const adminExists = admins.some(
+                    (admin) =>
+                    admin.username === newAdmin.username ||
+                    admin.email === newAdmin.email
+                    );
+                    if (adminExists) {
+                        console.error(
+                        "Admin with the same username or email already exists."
+                        );
+                        this.showWarning = true;
+                        return;
+                    }
+                    
+                    axios
+                    .get("http://localhost:3000/users")
                     .then((response) => {
-                        console.log(response.data)
-                        let to_edit = response.data.filter((admin) => admin.id == this.item_id)
-                        to_edit = to_edit[0]
-                        this.username = to_edit.username;
-                        this.password = to_edit.password;
-                        this.email = to_edit.email;
-                        this.telephone = to_edit.telephone;
-                        console.log(this.username)
-                    })
-                    .catch((error) => {
-                        console.error("Error registering admin:", error);
-                    });
-            }
-        },
-        registerAdmin() {
-            const newAdmin = {
-                username: this.username,
-                password: this.password,
-                email: this.email,
-                telephone: this.telephone
-            };
-
-            if (this.item_id === -1) {
-                // Check if username or email already exists in 'admin' or 'users' data
-                axios.get("http://localhost:3000/admin")
-                    .then((response) => {
-                        const admins = response.data;
-                        const adminExists = admins.some(admin => admin.username === newAdmin.username || admin.email === newAdmin.email);
-                        if (adminExists) {
-                            console.error("Admin with the same username or email already exists.");
-                            this.showWarning = true; // Set showWarning to true to display the warning text
+                        const users = response.data;
+                        const userExists = users.some(
+                        (user) =>
+                        user.username === newAdmin.username ||
+                        user.email === newAdmin.email
+                        );
+                        if (userExists) {
+                            console.error(
+                            "User with the same username or email already exists."
+                            );
+                            this.showWarning = true;
                             return;
                         }
-
-                        axios.get("http://localhost:3000/users")
-                            .then((response) => {
-                                const users = response.data;
-                                const userExists = users.some(user => user.username === newAdmin.username || user.email === newAdmin.email);
-                                if (userExists) {
-                                    console.error("User with the same username or email already exists.");
-                                    this.showWarning = true; // Set showWarning to true to display the warning text
-                                    return;
-                                }
-
-                                axios.post("http://localhost:3000/admin", newAdmin)
-                                    .then((response) => {
-                                        console.log("Admin registered successfully:", response.data);
-                                        this.username = "";
-                                        this.password = "";
-                                        this.email = "";
-                                        this.telephone = "";
-                                        this.showWarning = false; // Reset showWarning to false after successful registration
-                                    })
-                                    .catch((error) => {
-                                        console.error("Error registering admin:", error);
-                                    });
-                            })
-                            .catch((error) => {
-                                console.error("Error retrieving users:", error);
-                            });
+                        
+                        axios
+                        .post("http://localhost:3000/admin", newAdmin)
+                        .then((response) => {
+                            console.log(
+                            "Admin registered successfully:",
+                            response.data
+                            );
+                            this.username = "";
+                            this.password = "";
+                            this.email = "";
+                            this.telephone = "";
+                            this.showWarning = false;
+                        })
+                        .catch((error) => {
+                            console.error("Error registering admin:", error);
+                        });
                     })
                     .catch((error) => {
-                        console.error("Error retrieving admins:", error);
+                        console.error("Error retrieving users:", error);
                     });
-
+                })
+                .catch((error) => {
+                    console.error("Error retrieving admins:", error);
+                });
             }
             else {
-                axios.put(`http://localhost:3000/admin/${this.item_id}`, newAdmin)
-                    .then(response => {
-                        this.$router.push("/crud?crud=admin")
-                        // Faça algo com a resposta
-                    })
-                    .catch(error => {
-                        console.error('Erro ao atualizar o item:', error);
-                        // Trate o erro adequadamente
-                    });
+                alert("Fill in all necessary information");
             }
         },
     },
 };
-</script >
+</script>
+
 <style scoped>
 .container {
     display: flex;
@@ -209,7 +207,7 @@ input[type="telephone"] {
     width: 100%;
     padding: 5px 0;
     border: none;
-    border-bottom: 4px solid #46D115;
+    border-bottom: 4px solid #46d115;
     background-color: transparent;
     font-size: 16px;
     color: black;
@@ -217,12 +215,12 @@ input[type="telephone"] {
 
 .submit-button {
     margin: 20px;
-    background-color: #46D115;
+    background-color: #46d115;
     border-radius: 30px;
     height: 4vh;
     width: 30%;
     border: 0cm;
-    font-family: 'Courier New', Courier, monospace;
+    font-family: "Courier New", Courier, monospace;
     color: aliceblue;
     font-size: 18px;
     cursor: pointer;
--- src/views/RegisterClientView.vue
+++ src/views/RegisterClientView.vue
@@ -1,58 +1,87 @@
 <template>
     <Navbar />
-
+    
     <div class="container">
         <div class="box">
             <h2>Register Client</h2>
-
+            
             <div class="form-group">
                 <label for="username">Username:</label>
-                <input type="text" id="username" placeholder="Enter username" v-model="username" />
+                <input
+                type="text"
+                id="username"
+                placeholder="Enter username"
+                v-model="username"
+                required
+                />
                 <p class="warning-text" v-show="usernameExists">Username already exists.</p>
             </div>
-
+            
             <div class="form-group">
                 <label for="password">Password:</label>
-                <input type="password" id="password" placeholder="Enter password" v-model="password" />
+                <input
+                type="password"
+                id="password"
+                placeholder="Enter password"
+                v-model="password"
+                required
+                />
             </div>
-
+            
             <div class="form-group">
                 <label for="email">Email:</label>
-                <input type="email" id="email" placeholder="Enter email" v-model="email" />
+                <input
+                type="email"
+                id="email"
+                placeholder="Enter email"
+                v-model="email"
+                required
+                />
                 <p class="warning-text" v-show="emailExists">Email already exists.</p>
             </div>
-
+            
             <div class="form-group">
                 <label for="telephone">Telephone:</label>
-                <input type="tel" id="telephone" placeholder="Enter telephone" v-model="telephone" @input="formatPhoneNumber" maxlength="15"/>
+                <input
+                type="tel"
+                id="telephone"
+                placeholder="Enter telephone"
+                v-model="telephone"
+                @input="formatPhoneNumber"
+                maxlength="15"
+                required
+                />
             </div>
-
+            
             <div class="form-group">
                 <label for="address">Address:</label>
-                <input type="text" id="address" placeholder="Enter address" v-model="address" />
+                <input
+                type="text"
+                id="address"
+                placeholder="Enter address"
+                v-model="address"
+                required
+                />
             </div>
-
-
+            
             <button class="submit-button" @click="registerClient">Submit</button>
             <div v-show="success">
-                <p>User {{ this.username }} was registered ! </p>
+                <p>User {{ username }} was registered!</p>
                 <p>You will be redirected in <time><strong id="seconds">3</strong> seconds</time>.</p>
             </div>
-
-
         </div>
     </div>
 </template>
-<script>
 
-import Navbar from '../components/Navbar.vue'
-import Footer from '../components/Footer.vue'
-import axios from 'axios';
+<script>
+import Navbar from "../components/Navbar.vue";
+import Footer from "../components/Footer.vue";
+import axios from "axios";
 
 export default {
     components: {
         Navbar,
-        Footer
+        Footer,
     },
     name: "RegisterClient",
     data() {
@@ -62,141 +91,152 @@ export default {
             email: "",
             telephone: "",
             address: "",
-            usernameExists: false, // New data property for username existence check
-            emailExists: false, // New data property for email existence check
-
+            usernameExists: false,
+            emailExists: false,
             user: "",
-            success: false
+            success: false,
         };
     },
     props: {
         item_id: {
-            default: -1
-        }
+            default: -1,
+        },
     },
     mounted() {
-        this.load_edit()
+        this.load_edit();
     },
     methods: {
         formatPhoneNumber() {
-            let value = this.telephone
-            value = value.replace(/\D/g, '')
-            value = value.replace(/(\d{2})(\d)/, "($1) $2")
-            value = value.replace(/(\d)(\d{4})$/, "$1-$2")
-            this.telephone = value
+            let value = this.telephone;
+            value = value.replace(/\D/g, "");
+            value = value.replace(/(\d{2})(\d)/, "($1) $2");
+            value = value.replace(/(\d)(\d{4})$/, "$1-$2");
+            this.telephone = value;
         },
         countdown() {
-            var el = document.getElementById('seconds'),
-                total = el.innerHTML,
-                timeinterval = setInterval(function () {
-                    total = --total;
-                    el.textContent = total;
-                    if (total <= 0) {
-                        clearInterval(timeinterval);
-                    }
-                }, 1000);
-
+            var el = document.getElementById("seconds"),
+            total = el.innerHTML,
+            timeinterval = setInterval(function () {
+                total = --total;
+                el.textContent = total;
+                if (total <= 0) {
+                    clearInterval(timeinterval);
+                }
+            }, 1000);
+            
             setTimeout(() => {
-                this.$router.push("/login")
+                this.$router.push("/login");
             }, 3000);
         },
         load_edit() {
-            console.log(this.item_id)
+            console.log(this.item_id);
             if (this.item_id !== -1) {
-                axios.get("http://localhost:3000/users/" + this.item_id)
-                    .then((response) => {
-                        this.username = response.data.username
-                        this.password = response.data.password
-                        this.email = response.data.email
-                        this.telephone = response.data.telephone
-                        this.address = response.data.address
-                    })
-                    .catch((error) => {
-                        console.error("Error registering admin:", error);
-                    });
+                axios
+                .get("http://localhost:3000/users/" + this.item_id)
+                .then((response) => {
+                    this.username = response.data.username;
+                    this.password = response.data.password;
+                    this.email = response.data.email;
+                    this.telephone = response.data.telephone;
+                    this.address = response.data.address;
+                })
+                .catch((error) => {
+                    console.error("Error registering admin:", error);
+                });
             }
         },
         registerClient() {
-            const newClient = {
-                username: this.username,
-                password: this.password,
-                email: this.email,
-                telephone: this.telephone,
-                address: this.address,
-            };
-
-            if (this.item_id == -1) {
-
-
-                axios.get("http://localhost:3000/admin")
+            if (
+            this.username != "" &&
+            this.password != "" &&
+            this.email != "" &&
+            this.telephone != "" &&
+            this.address != ""
+            )  {
+                const newClient = {
+                    username: this.username,
+                    password: this.password,
+                    email: this.email,
+                    telephone: this.telephone,
+                    address: this.address,
+                };
+                
+                if (this.item_id == -1) {
+                    axios
+                    .get("http://localhost:3000/admin")
                     .then((adminResponse) => {
-                        axios.get("http://localhost:3000/users")
-                            .then((usersResponse) => {
-                                const admins = adminResponse.data;
-                                const users = usersResponse.data;
-                                const usernameExists =
-                                    admins.some((admin) => admin.username === newClient.username) ||
-                                    users.some((user) => user.username === newClient.username);
-                                const emailExists =
-                                    admins.some((admin) => admin.email === newClient.email) ||
-                                    users.some((user) => user.email === newClient.email);
-                                if (usernameExists) {
-                                    this.usernameExists = true;
-                                    this.emailExists = false;
-                                    return;
-                                }
-                                if (emailExists) {
-                                    this.usernameExists = false;
-                                    this.emailExists = true;
-                                    return;
-                                }
-
-                                axios.post("http://localhost:3000/users", newClient)
-                                    .then((response) => {
-                                        this.user;
-
-
-                                        console.log("Client registered:", response.data);
-                                        this.username = "";
-                                        this.password = "";
-                                        this.email = "";
-                                        this.telephone = "";
-                                        this.address = "";
-                                        this.usernameExists = false;
-                                        this.emailExists = false;
-
-                                        this.success = true;
-
-                                        this.countdown();
-
-                                    })
-                                    .catch((error) => {
-                                        console.log(error);
-                                    });
+                        axios
+                        .get("http://localhost:3000/users")
+                        .then((usersResponse) => {
+                            const admins = adminResponse.data;
+                            const users = usersResponse.data;
+                            const usernameExists =
+                            admins.some((admin) => admin.username === newClient.username) ||
+                            users.some((user) => user.username === newClient.username);
+                            const emailExists =
+                            admins.some((admin) => admin.email === newClient.email) ||
+                            users.some((user) => user.email === newClient.email);
+                            if (usernameExists) {
+                                this.usernameExists = true;
+                                this.emailExists = false;
+                                return;
+                            }
+                            if (emailExists) {
+                                this.usernameExists = false;
+                                this.emailExists = true;
+                                return;
+                            }
+                            
+                            axios
+                            .post("http://localhost:3000/users", newClient)
+                            .then((response) => {
+                                this.user;
+                                
+                                console.log("Client registered:", response.data);
+                                this.username = "";
+                                this.password = "";
+                                this.email = "";
+                                this.telephone = "";
+                                this.address = "";
+                                this.usernameExists = false;
+                                this.emailExists = false;
+                                
+                                this.success = true;
+                                
+                                this.countdown();
                             })
                             .catch((error) => {
                                 console.log(error);
                             });
+                        })
+                        .catch((error) => {
+                            console.log(error);
+                        });
                     })
                     .catch((error) => {
                         console.log(error);
                     });
-
-            } else {
-                axios.put(`http://localhost:3000/users/${this.item_id}`, newClient)
-                    .then(response => {
-                        this.$router.push("/crud?crud=users")
-                        // Faça algo com a resposta
+                } else {
+                    axios
+                    .put(`http://localhost:3000/users/${this.item_id}`, newClient)
+                    .then((response) => {
+                        this.$router.push("/crud?crud=users");
+                        // Do something with the response
                     })
-                    .catch(error => {
-                        console.error('Erro ao atualizar o item:', error);
-                        // Trate o erro adequadamente
+                    .catch((error) => {
+                        console.error("Error updating item:", error);
+                        // Handle the error properly
                     });
+                }
             }
-        }
+            else {
+                alert("Fill in all necessary information");
+            }
+        },
     },
 };
-</script >
+</script>
+
 <style scoped>
 .container {
     margin-top: 15vh;
